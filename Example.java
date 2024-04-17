5cb128115c5dcfb5d1cbeb7e3ca59255e57cc847
Lucas Weckermann <lucas.weckermann@vit-bund.de>
2024-04-17 22:27:09 +0200

    Komma durch Punkt ersetzt, da hier die englische Notation verwendet werden muss

--- Example.java
+++ Example.java
@@ -1,7 +1,7 @@
 public class Example {
 	public static void main(String[] args) {
 		int i = 0;
-		double pi = 3,141592653589793238462643383;
+		double pi = 3.141592653589793238462643383;
 		System.out.println("Zahlen über Zahlen");
 	}
 }
